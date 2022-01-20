8eae7232eab173dffb52d014ff018d990c4e85a9
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2022-01-20 12:56:33 -0500

    changed budget path to transactions path and now FE not working

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 // DEPENDENCIES
 const express = require("express");
-const cors = require("cors");
-const budgetController = require("./controllers/budgetControllers.js");
-
-// CONFIGURATION
 const app = express();
+const transactionsController = require("./controllers/transactionsControllers.js");
+const cors = require("cors");
 
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-app.use("/budget", budgetController);
 
 // ROUTES
 app.get("/", (req,res) => {
     res.send("Hello World");
 });
+
+app.use("/transactions", transactionsController);
 
 app.get("*", (req, res) => {
     res.status(404).json({error: "Page not found"});
---
+++ controllers/transactionsControllers.js
@@ -0,0 +1,56 @@
+// DEPENDENCIES
+const express = require("express");
+const transactionRoutes = express.Router();
+const transactionsArr = require("../models/transactions.js");
+
+// CONTROLLER ROUTES
+
+// Contains entire "models" index
+transactionRoutes.get("/", (req,res) => {
+    res.json(transactionsArr);
+});
+
+transactionRoutes.get("/error", (req,res) => {
+    res.send(`<h1>404! Page not found</h1>`);
+});
+
+// Shows only the indiviual index from the "models" 
+transactionRoutes.get("/:index", (req, res) => {
+    const { index } = req.params;
+    if(transactionsArr[index]){
+        return res.json(transactionsArr[index]);
+    }
+    res.status(404).redirect("/error");
+});
+
+// Adds a new indiviual index into the existing "models"
+transactionRoutes.post("/", (req, res) => {
+    transactionsArr.push(req.body);
+    res.json(transactionsArr[transactionsArr.length - 1]);
+});
+
+// Deletes a indiviual index from the existing "models"
+transactionRoutes.delete("/:index", (req, res) => {
+    const { index } = req.params;
+    let removed = transactionsArr.splice(index, 1);
+    transactionsArr[index] ? res.json(removed[0]) : res.status(404).redirect("/error");
+});
+
+// Allows you to update an existing index from the "models"
+transactionRoutes.put("/:index", (req, res) => {
+    const { index } = req.params;
+    if(!transactionsArr[index]){
+        return res.status(422).json({error: "Not found"});
+    }
+
+    let { item_name, amount, date, from, category } = req.body;
+    if(item_name && amount && date && from !== undefined && category){
+        transactionsArr[index] = {
+            item_name, amount, date, from, category
+        };
+        return res.json(transactionsArr[index]);
+    }
+    res.status(422).json({error: "Please provide all fields"});
+});
+
+module.exports = transactionRoutes;
