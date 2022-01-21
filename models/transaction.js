b25ad6cbe1306e5e468ed7739d2cf8e02bbe4abe
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2022-01-21 16:35:16 -0500

    Back End commit for heroku build

--- models/transaction.js
+++ models/transaction.js
@@ -1,51 +1,51 @@
 module.exports = [
     {
-      date: "April 1",
-      item_name: "Groceries",
-      amount: -30,
-      from: "Ideal",
-      category: ""
-    },
-    {
-      date: "April 1",
-      item_name: "Shopping",
-      amount: -47,
-      from: "Target",
-      category: ""
-    },
-    {
-      date: "April 2",
-      item_name: "Income",
-      amount: 2000,
-      from: "Google",
-      category: ""
-    },
-    {
-      date: "April 5",
-      item_name: "Gas",
-      amount: -25,
-      from: "Shell Gas Station",
-      category: ""
-    },
-    {
-      date: "April 6",
+      date: "January 1",
       item_name: "Rent",
-      amount: -700,
-      from: "Pike International, LLC",
+      amount: -500,
+      from: "Hector's bank account 🥲",
       category: ""
     },
     {
-      date: "April 8",
+      date: "January 15",
+      item_name: "Monthly Subscriptions",
+      amount: -50,
+      from: "Spotify, Netflix, Hulu, etc., etc.",
+      category: ""
+    },
+    {
+      date: "January 16",
       item_name: "Income",
       amount: 2000,
       from: "Google",
       category: ""
     },
     {
-      date: "April 8",
+      date: "January 16",
+      item_name: "Savings",
+      amount: -1000,
+      from: "Chase",
+      category: ""
+    },
+    {
+      date: "January 17",
+      item_name: "Credit Card Payment",
+      amount: -200,
+      from: "Capital One",
+      category: ""
+    },
+    {
+      date: "January 18",
       item_name: "Groceries",
-      amount: -40,
-      from: "Mr. Kiwi",
+      amount: -150,
+      from: "Key Foods",
+      category: ""
+    },
+    {
+      date: "January 23",
+      item_name: "Internet & Phone",
+      amount: -220,
+      from: "Verizon",
       category: ""
     },
   ];
